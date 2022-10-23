232d432ac56bba1e88663c234c419d0d47a9dc87
Daniel Wolter <wolter.daniel@gmail.com>
2022-10-23 17:03:17 -0300

    testes com problemas

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import RESPONSE_MOCK from './mocks/testData';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
-import { AppContext } from "../context/AppContext";
-import { userEvent } from "@testing-library/user-event";
+import RESPONSE_MOCK from "./mocks/testData";
 
-describe("App", () => {
-  beforeEach (async () => {
-    global.fetch = jest.fn(async () =>
-      Promise.resolve({json: async () => Promise.resolve(RESPONSE_MOCK),}));
-    render(
-      <PlanetsProvider>
-        <App />
-      </PlanetsProvider>
-    );
 
-    await waitForElementToBeRemoved(() => screen.getByText(/Loading.../i));
-  });
+beforeEach(() => {
+  global.fetch = jest.fn(async () =>
+    Promise.resolve({
+      json: async () => Promise.resolve(RESPONSE_MOCK),
+    })
+  );
 
-  it("1. should call api on mount", async () => {
-    const endpoint = "https://swapi.dev/api/planets";
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-    expect(global.fetch).toHaveBeenCalledWith(endpoint);
+  await waitForElementToBeRemoved(() => screen.getByText(/Loading.../i));
+});
+
+describe("Testando Star Wars Planes Api", () => {
+  test("se a tabela esta renderizada", () => {
+    render(<App />);
+    const tabela = screen.getByRole("table");
+    expect(tabela).toBeInTheDocument();
   });
 
 });
